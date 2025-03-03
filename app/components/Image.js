45b111a11d1422f98ba9e8ccbf636fd023a5d639
Leopg21 <136273381+Leopg21@users.noreply.github.com>
2025-03-03 01:12:57 +0000

    Update Image.js
    image error

--- app/components/Image.js
+++ app/components/Image.js
@@ -1,4 +1,4 @@
 // Image.js
 export default function ImageComponent({ src, alt }) {
-    return <img className="rounded-lg shadow-md" src={src} alt={alt} />;
+    return <Image className="rounded-lg shadow-md" src={src} alt={alt} />;
   }
