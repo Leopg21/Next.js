fbaa5352f26b438c85975bde19a8f079f1e94b4e
Leopg21 <136273381+Leopg21@users.noreply.github.com>
2025-03-03 01:49:15 +0000

    Fixed JSX apostrophe errors and replaced <img> with <Image />

--- app/components/Image.js
+++ app/components/Image.js
@@ -1,4 +1,5 @@
 // Image.js
+import Image from 'next/image';
 export default function ImageComponent({ src, alt }) {
-    return <img className="rounded-lg shadow-md" src={src} alt={alt} />;
+    return <Image className="rounded-lg shadow-md" src={src} alt={alt} />;
   }
